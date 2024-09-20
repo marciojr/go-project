e2281b277c316f437297cfedd78c762fcd856f15
marciojr <marciomendescjr@gmail.com>
2024-09-19 22:54:40 -0300

    feat: add error struts and types

---
+++ src/configuration/rest_err/rest_err.go
@@ -0,0 +1,92 @@
+package rest_err
+
+import "net/http"
+
+type RestErr struct {
+	Message string   `json:"message"`
+	Err     string   `json:"error"`
+	Code    int64    `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func NewRestErr(message, err string, code int64, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal server error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewInternalServerValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal server error",
+		Code:    http.StatusInternalServerError,
+		Causes:  causes,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewNotFoundValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not found",
+		Code:    http.StatusNotFound,
+		Causes:  causes,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "Forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
+
+func NewForbiddenValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "Forbidden",
+		Code:    http.StatusForbidden,
+		Causes:  causes,
+	}
+}
