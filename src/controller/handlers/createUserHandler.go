dc7ec9a4a63a9228e60a95f70920eccd2db58510
marciojr <marciomendescjr@gmail.com>
2024-09-21 16:24:40 -0300

    feat: create response, request and use validator to check fields

--- src/controller/handlers/createUserHandler.go
+++ src/controller/handlers/createUserHandler.go
@@ -1,7 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/marciojr/go-project/src/configuration/rest_err"
+	"github.com/marciojr/go-project/src/controller/model/request"
 )
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrects fields, error: %s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
