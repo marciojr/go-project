7101dbf310083d43c155862e3f5889da508eb155
marciojr <marciomendescjr@gmail.com>
2024-09-20 23:14:55 -0300

    feat: add routers and userHandlers

---
+++ src/controller/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/marciojr/go-project/src/controller/userHandlers"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+	r.GET("/users", controller.FindAllUsers)
+	r.GET("/users/:id", controller.FindUserById)
+	r.GET("/users/email/:email", controller.FindUserByEmail)
+	r.POST("/users", controller.CreateUser)
+	r.PUT("/users", controller.UpdateUser)
+	r.DELETE("/users", controller.DeleteUser)
+}
---
+++ src/controller/userHandlers/findUser.go
@@ -0,0 +1,9 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func FindAllUsers(c *gin.Context) {}
+
+func FindUserById(c *gin.Context) {}
+
+func FindUserByEmail(c *gin.Context) {}
