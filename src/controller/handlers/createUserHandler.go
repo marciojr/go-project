94f11dc53245f61e7ac37d1367733cc0beb4e798
marciojr <marciomendescjr@gmail.com>
2024-09-21 22:15:30 -0300

    feat: use new model by UserDomainModelInteface to createUser

--- src/controller/handlers/createUserHandler.go
+++ src/controller/handlers/createUserHandler.go
@@ -1,11 +1,17 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/marciojr/go-project/src/configuration/validation"
 	"github.com/marciojr/go-project/src/controller/model/request"
+	"github.com/marciojr/go-project/src/controller/model/response"
+	"github.com/marciojr/go-project/src/model"
+)
+
+var (
+	UserDomainInterface model.UserDomainInterface
 )
 
 func CreateUser(c *gin.Context) {
@@ -19,5 +25,24 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	fmt.Println(userRequest)
+	domain := model.NewUserDomain(
+		userRequest.Email,
+		userRequest.Password,
+		userRequest.Name,
+		userRequest.Age,
+	)
+
+	if err := domain.CreateUser(); err != nil {
+		c.JSON(err.Code, err)
+		return
+	}
+
+	response := response.UserResponse{
+		ID:    "1",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(http.StatusOK, response)
 }
