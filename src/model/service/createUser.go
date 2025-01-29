3b880ff47bd7040c185e7289f67c3b1339225608
marciojr <marciomendescjr@gmail.com>
2025-01-29 00:18:21 -0300

    fix: fix create user feature to avoid send error all the time

--- src/model/service/createUser.go
+++ src/model/service/createUser.go
@@ -7,14 +7,10 @@ import (
 
 func (ud *userDomainService) CreateUser(userDomain model.UserDomainInterface) (model.UserDomainInterface, *rest_err.RestErr) {
 
-	user, err := ud.FindUserByEmail(userDomain.GetEmail())
+	user, _ := ud.FindUserByEmail(userDomain.GetEmail())
 
 	if user != nil {
 		return nil, rest_err.NewBadRequestError("Email already exists")
-	}
-
-	if err != nil {
-		return nil, err
 	}
 
 	userDomain.EncryptPassword()
